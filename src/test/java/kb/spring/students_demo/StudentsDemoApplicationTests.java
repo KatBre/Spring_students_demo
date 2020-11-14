30a8787dbfd769cc38447a24bea66892d0b909e9
Katarzyna Brewinska <brewinska.katarzyna@gmail.com>
2020-11-14 13:31:08 +0100

    learning spring initial commit

---
+++ src/test/java/kb/spring/students_demo/StudentsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kb.spring.students_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
